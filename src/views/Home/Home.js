7626353f9ca557c8bd8504ad3d6fa2533ce6a2a3
Blake Cannell <me@blakecannell.com>
2018-05-21 11:47:58 +1000

    Sprint #2: Home page cleanup

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -58,19 +58,23 @@ class Home extends Component<Props, State> {
   render() {
     return (
       <section className="Home animated fadeIn clearfix">
-        <Col xs={10} xsOffset={1} sm={5} smOffset={1} className="half-screen">
-          <article>
-            <h1>Collaborate</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
-            <LinkContainer to="/collaborate"><span className="btn">Create</span></LinkContainer>
-          </article>
+        <Col xs={12} sm={6} className="half-screen">
+          <Col xs={10} xsOffset={1}>
+            <article>
+              <h1>Collaborate</h1>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
+              <LinkContainer to="/collaborate"><span className="btn">Create</span></LinkContainer>
+            </article>
+          </Col>
         </Col>
-        <Col xs={10} xsOffset={1} sm={5} smOffset={0} className="half-screen">
-          <article>
-            <h1>Listen</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
-            <LinkContainer to="/listen"><span className="btn">Play</span></LinkContainer>
-          </article>
+        <Col xs={12} sm={6} className="half-screen">
+          <Col xs={10} xsOffset={1}>
+            <article>
+              <h1>Listen</h1>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
+              <LinkContainer to="/listen"><span className="btn">Play</span></LinkContainer>
+            </article>
+          </Col>
         </Col>
       </section>
     );
