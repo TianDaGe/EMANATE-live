fc604cbf1128e763505d03f383ddffe535b100b5
Blake Cannell <me@blakecannell.com>
2018-05-21 11:49:09 +1000

    Sprint #2: Clear stale functions from home (unused)

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -6,55 +6,10 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Col } from 'react-bootstrap';
 import './Home.css';
 
-
-type Props = {
-
-  intl: intlShape
-};
-
-type State = {
-  muted: boolean
-};
+type Props = {};
+type State = {};
 
 class Home extends Component<Props, State> {
-  state = {
-    muted: true
-  };
-
-  videoElem: ?HTMLVideoElement;
-
-
-  fadeMute(muted: boolean, volume: ?number) {
-    if(!this.videoElem) return;
-
-    if(volume === undefined) {
-      volume = muted ? 1 : 0;
-      this.videoElem.volume = volume;
-      if(!muted) this.videoElem.muted = muted;
-      setTimeout(() => this.fadeMute(muted, volume), 20);
-    } else {
-      volume += muted ? -0.1 : 0.1;
-      if(volume < 0 || volume > 1) {
-        if(muted) this.videoElem.muted = muted;
-        return;
-      } else {
-        this.videoElem.volume = volume;
-        setTimeout(() => this.fadeMute(muted, volume), 20);
-      }
-    }
-  }
-
-  toggleMute() {
-    const muted = !this.state.muted;
-
-    //this.videoElem.muted = muted;
-    setTimeout(() => this.fadeMute(muted), 0);
-
-    this.setState({
-      muted
-    });
-  }
-
   render() {
     return (
       <section className="Home animated fadeIn clearfix">
